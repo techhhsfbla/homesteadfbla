f6fbffcc6ad980b22ce7df0a613866e33844d6e5
Alessandro Montesi <alessandromontesi234@gmail.com>
2026-09-15 14:54:38 -0700

    Update profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -27,7 +27,7 @@ const ProfileCard = () => {
   const milestones = [
     { name: 'Bronze Member', points: 10, color: '#CD7F32', prize: 'None'},
     { name: 'Silver Member', points: 25, color: '#A8A8A8', prize: 'Top Member Party *Applies to First Semester*' },
-    { name: 'Gold Member', points: 40, color: '#E5E4E2', prize: 'Choose SLC bus' },
+    { name: 'Gold Member', points: 40, color: '#E5E4E2', prize: 'Choose BSLC bus' },
     { name: 'Diamond Member', points: 50, color: '#00BFFF', prize: '???' }
   ];
 
